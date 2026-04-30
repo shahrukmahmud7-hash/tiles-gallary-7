d3e24230d85bf767490de2509e94da6d2abc0f82
shahrukmahmud7-hash <shahrukmahmud7@gmail.com>
2026-05-01 02:12:41 +0600

    navbar and banner done

---
+++ src/components/Banner.jsx
@@ -0,0 +1,39 @@
+import { Button } from "@heroui/react";
+import Link from "next/link";
+
+const Banner = () => {
+  return (
+    <div className="max-w-7xl mx-auto"> 
+    <div className="bg-[url('https://i.ibb.co.com/jvMyP74r/tiles-showroom-design-3d-rendering-1001861-114.avif')] h-[40vh] sm:h-[50vh] md:h-[55vh] lg:h-[60vh] w-full bg-cover bg-no-repeat bg-center flex items-center rounded-lg shadow-2xl lg: ">
+      {/* Overlay */}
+      <div className="w-full h-full rounded-lg bg-black/50 flex items-center ">
+        <div className="max-w-7xl mx-auto px-6 text-white">
+          <h1 className="text-4xl md:text-5xl text-gray-50 font-bold mb-4 max-w-2xl">
+            Browse a wide range of tiles To bring your ideas to life.
+          </h1>
+          <p className="text-lg md:text-xl mb-6 max-w-xl text-gray-200">
+            Explore unique patterns and textures that bring life and character to your space.
+          </p>
+
+          <div className="flex gap-4">
+            <Link href="#">
+             <Button className="bg-gradient-to-r from-red-500 via-red-800 to-red-950
+              text-white font-semibold shadow-md hover:opacity-90 transition">
+                   Discover Now
+           </Button>
+            </Link>
+
+            <Link href="/pricing">
+              <Button variant="outline" className="text-white">
+                View Gallery 
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="border-b px-2 sticky top-0 z-50 bg-white shadow-md ">
+      <nav className="flex justify-between items-center py-3 max-w-7xl mx-auto w-full ">
+
+        {/* Logo here */}
+        <div className="flex gap-2 items-center">
+          <Image
+            src={"/tile-logo2.jpg"}
+            alt="logo"
+            width={60}
+            height={60}
+          />
+          <h3 className="font-black text-lg bg-gradient-to-r from-slate-800 via-blue-600 to-slate-500 bg-clip-text text-transparent "
+          >
+            Tiles Gallery</h3>
+        </div>
+
+        {/* Nav all linlks are here */}
+        <ul className="flex items-center gap-5 text-[#706F6F] text-sm mx-auto w-fit">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/all-tiles">All Tiles</Link>
+          </li>
+          <li>
+            <Link href="/my-profile">My Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
