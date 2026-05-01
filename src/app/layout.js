3521fb2b8ad30dea7dcda6a0258d160aa4fb3362
shahrukmahmud7-hash <shahrukmahmud7@gmail.com>
2026-05-01 06:38:00 +0600

    deply check

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const OutfitFont = Outfit({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata = {
   title: "Tiles-Gallery",
@@ -22,17 +18,15 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${OutfitFont.className}  h-full antialiased`}
     >
-      <body className="">
-
-        <main className=" ">
-          <Navbar/>
-          {children}
-           <Footer/>
-        </main>
+      <body>
+        <Navbar/>
+        <main className="max-w-7xl mx-auto">{children} </main>
+        <Footer/>
 
       </body>
     </html>
   );
 }
+
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 
 const Banner = () => {
   return (
-    <div className="max-w-7xl mx-auto"> 
+    <div className="max-w-7xl mx-auto "> 
     <div className="bg-[url('https://i.ibb.co.com/jvMyP74r/tiles-showroom-design-3d-rendering-1001861-114.avif')] h-[40vh] sm:h-[50vh] md:h-[55vh] lg:h-[60vh] w-full bg-cover bg-no-repeat bg-center flex items-center rounded-lg shadow-2xl lg: ">
       {/* Overlay */}
       <div className="w-full h-full rounded-lg bg-black/50 flex items-center ">
         <div className="max-w-7xl mx-auto px-6 text-white">
           <h1 className="text-4xl md:text-5xl text-gray-50 font-bold mb-4 max-w-2xl">
-            Browse a wide range of tiles To bring your ideas to life.
+            Browse a wide range of tiles To bring your ideas to life.!
           </h1>
           <p className="text-lg md:text-xl mb-6 max-w-xl text-gray-200">
             Explore unique patterns and textures that bring life and character to your space.
           </p>
 
-          <div className="flex gap-4">
+          <div className="flex gap-4 ">
             <Link href="#">
              <Button className="bg-gradient-to-r from-red-500 via-red-800 to-red-950
               text-white font-semibold shadow-md hover:opacity-90 transition">
@@ -36,4 +36,4 @@ const Banner = () => {
   );
 };
 
-export default Banner;
+export default Banner; 
